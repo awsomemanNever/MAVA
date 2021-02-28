a3060283b5d366aa4d00f2e4cc1981a289ba1146
Blue Chalk <thebluechalk@gmail.com>
2021-02-28 13:00:07 +0530

    redid basic funtionality with refactoring

--- main.py
+++ main.py
@@ -6,43 +6,32 @@ import datetime
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            listener.adjust_for_ambient_noise(source)
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'monday' in command:
+                command = command.replace('monday', '')
+                interpret(command)
+    except:
+        pass
+    return command
+
+
+def interpret(command):
+    if 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+
 def main():
-    
     while True:
-        recognise()
-
-def recognise() : 
-
-    with sr.Microphone() as source:
-        print("listening")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        
-        command = command.lower()
-        command = command.split(" ")
-        
-        
-        if 'monday' == command[0]:
-            command.pop(0) 
-            command = "".join(command)
-
-            if "play" in command:
-                play(command)
-
-            elif "time" in command:
-                time(command)
-                
-            
-            
-def play(command): 
-    command.pop(0)
-    engine.say("Playing" + command)
-    pywhatkit.playonyt(command)
-    engine.runAndWait()
-
-def time(command):
-    engine.say("The time is" + datetime.datetime.now().strftime('%I:%M %p'))
-    engine.runAndWait()
-
-
-main()
+        take_command()
